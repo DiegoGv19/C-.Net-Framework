019bde9dae77cc37728312ddc73a202749aa4773
Diego <diego_13_1999@hotmail.com>
2020-02-01 17:44:35 -0500

    commi  se muestra los datos de la base de datos, pero falta terminar el diseño con css

---
+++ Data.gov.sg/Data.gov.sg/Controllers/IndexController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Data.gov.sg.Models;
+
+namespace Data.gov.sg.Controllers
+{
+    public class IndexController : Controller
+    {
+
+
+        public ActionResult Tabla()
+        {
+
+            DataGovSgEntities db = new DataGovSgEntities();
+
+            List<Graduates_From_University> lista = db.Graduates_From_University.ToList();
+            return View(lista);
+        }
+
+    }
+}
